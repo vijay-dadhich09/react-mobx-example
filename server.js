2d9e337d5d4738e0c9ca3b12ec0f39ef7bb04f52
vijay-dadhich09 <vijay.dadhich09@gmail.com>
2017-08-11 14:15:13 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+<<<<<<< .mine
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(5050, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:5050');
+});
+=======
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
@@ -12,4 +29,4 @@ new WebpackDevServer(webpack(config), {
     }
 
     console.log('Listening at localhost:4040');
-});
+});>>>>>>> .r5
