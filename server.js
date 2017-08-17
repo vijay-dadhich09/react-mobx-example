ad600a2fd3509981f964aafff0724a4fe57dbf43
Vijay Shanker Dadhich <vdadhich@sapient.com>
2017-08-17 13:05:27 +0530

    update server.js file

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-<<<<<<< .mine
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
-
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(5050, 'localhost', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
-
-  console.log('Listening at localhost:5050');
-});
-=======
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
@@ -29,4 +12,4 @@ new WebpackDevServer(webpack(config), {
     }
 
     console.log('Listening at localhost:4040');
-});>>>>>>> .r5
+});
